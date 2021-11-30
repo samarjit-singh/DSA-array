7e36fd5122e105dfbd5a326ecaf805677c0743ab
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2021-11-30 23:28:22 +0530

    minimize the height

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,1 @@
-// first print the elements of array1
-  for(int i=0;i<n;i++){
-    cout<<arr1[i]<<" ";
-  }
-  // check for the second array 
-  // if elemtns in array 2 are not same as array 1 the print them
-  for(int j=0;j<m;j++){
-    int flag = 0;
-    for(int i=0;i<n;i++){
-      if(arr1[i]==arr2[j]){
-        flag=1;
-        break;
-      }
-    }
-    if(flag!=1){
-      cout<<arr2[j]<<" ";
-    }
-  }
+*min_element(arr, arr + n);
